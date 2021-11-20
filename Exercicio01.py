878ae57fb78790e408dd3bf756f60d656f4a451c
Evaldo <evaldo1706@gmail.com>
2021-11-20 19:22:35 -0300

    Primeiro Exercicio

---
+++ Exercicio01.py
@@ -0,0 +1,11 @@
+#primeira versão de exercicio
+x = float(input('Digite um valor para x: '))
+y = float(input('Digite um valor para y: '))
+soma = x + y
+produto = x*y
+sub = x - y
+divisao = x/y
+print(f'soma de {x} + {y} = {soma}')
+print(f'subtração de {x} - {y} = {sub}')
+print(f'Multiplicação de {x} x {y} = {produto}')
+print(f'divisão de {x} : {y} = {divisao}')
